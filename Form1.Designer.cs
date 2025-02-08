000d6e9652253b13c5a6665f9902ab12bcb47472
jeffpeng3 <jeffpeng2012@gmail.com>
2025-02-08 22:49:43 +0800

    feat: add frame selector
    use test data provider

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,146 +1,197 @@
 ﻿namespace MyPic_Annotator
 {
-    partial class Form1
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            picture = new PictureBox();
-            next = new Button();
-            nextUnannotate = new Button();
-            prevUnannotate = new Button();
-            prev = new Button();
-            save = new Button();
-            export = new Button();
-            ((System.ComponentModel.ISupportInitialize)picture).BeginInit();
-            SuspendLayout();
-            // 
-            // picture
-            // 
-            picture.Location = new Point(12, 12);
-            picture.Name = "picture";
-            picture.Size = new Size(640, 360);
-            picture.SizeMode = PictureBoxSizeMode.Zoom;
-            picture.TabIndex = 0;
-            picture.TabStop = false;
-            // 
-            // next
-            // 
-            next.Location = new Point(512, 546);
-            next.Name = "next";
-            next.Size = new Size(138, 23);
-            next.TabIndex = 2;
-            next.Text = "Next (L)";
-            next.UseVisualStyleBackColor = true;
-            next.Click += next_Click;
-            // 
-            // nextUnannotate
-            // 
-            nextUnannotate.Location = new Point(512, 517);
-            nextUnannotate.Name = "nextUnannotate";
-            nextUnannotate.Size = new Size(138, 23);
-            nextUnannotate.TabIndex = 3;
-            nextUnannotate.Text = "NextUnannotate (O)";
-            nextUnannotate.UseVisualStyleBackColor = true;
-            nextUnannotate.Click += nextUnannotate_Click;
-            // 
-            // prevUnannotate
-            // 
-            prevUnannotate.Location = new Point(12, 517);
-            prevUnannotate.Name = "prevUnannotate";
-            prevUnannotate.Size = new Size(138, 23);
-            prevUnannotate.TabIndex = 5;
-            prevUnannotate.Text = "PrevUnannotate (U)";
-            prevUnannotate.UseVisualStyleBackColor = true;
-            prevUnannotate.Click += prevUnannotate_Click;
-            // 
-            // prev
-            // 
-            prev.Location = new Point(12, 546);
-            prev.Name = "prev";
-            prev.Size = new Size(138, 23);
-            prev.TabIndex = 6;
-            prev.Text = "Prev (J)";
-            prev.UseVisualStyleBackColor = true;
-            prev.Click += prev_Click;
-            // 
-            // save
-            // 
-            save.Location = new Point(275, 517);
-            save.Name = "save";
-            save.Size = new Size(138, 23);
-            save.TabIndex = 7;
-            save.Text = "Save (Enter)";
-            save.UseVisualStyleBackColor = true;
-            save.Click += save_Click;
-            // 
-            // export
-            // 
-            export.Location = new Point(275, 546);
-            export.Name = "export";
-            export.Size = new Size(138, 23);
-            export.TabIndex = 8;
-            export.Text = "Export";
-            export.UseVisualStyleBackColor = true;
-            export.Click += export_Click;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            AutoSize = true;
-            ClientSize = new Size(662, 581);
-            Controls.Add(export);
-            Controls.Add(save);
-            Controls.Add(prev);
-            Controls.Add(prevUnannotate);
-            Controls.Add(nextUnannotate);
-            Controls.Add(next);
-            Controls.Add(picture);
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            MaximizeBox = false;
-            MinimizeBox = false;
-            Name = "Form1";
-            Text = "Form1";
-            FormClosing += Form1_FormClosing;
-            Load += Form1_Load;
-            KeyDown += Form1_KeyDown;
-            ((System.ComponentModel.ISupportInitialize)picture).EndInit();
-            ResumeLayout(false);
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			picture = new PictureBox();
+			next = new Button();
+			nextUnannotate = new Button();
+			prevUnannotate = new Button();
+			prev = new Button();
+			save = new Button();
+			export = new Button();
+			subtitleLabel = new Label();
+			frameLabel = new Label();
+			anotateStatus = new Label();
+			numericUpDown1 = new NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)picture).BeginInit();
+			((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+			SuspendLayout();
+			// 
+			// picture
+			// 
+			picture.Location = new Point(12, 12);
+			picture.Name = "picture";
+			picture.Size = new Size(640, 360);
+			picture.SizeMode = PictureBoxSizeMode.Zoom;
+			picture.TabIndex = 0;
+			picture.TabStop = false;
+			// 
+			// next
+			// 
+			next.Location = new Point(512, 546);
+			next.Name = "next";
+			next.Size = new Size(138, 23);
+			next.TabIndex = 2;
+			next.Text = "Next (L)";
+			next.UseVisualStyleBackColor = true;
+			next.Click += next_Click;
+			// 
+			// nextUnannotate
+			// 
+			nextUnannotate.Location = new Point(512, 517);
+			nextUnannotate.Name = "nextUnannotate";
+			nextUnannotate.Size = new Size(138, 23);
+			nextUnannotate.TabIndex = 3;
+			nextUnannotate.Text = "NextUnannotate (O)";
+			nextUnannotate.UseVisualStyleBackColor = true;
+			nextUnannotate.Click += nextUnannotate_Click;
+			// 
+			// prevUnannotate
+			// 
+			prevUnannotate.Location = new Point(12, 517);
+			prevUnannotate.Name = "prevUnannotate";
+			prevUnannotate.Size = new Size(138, 23);
+			prevUnannotate.TabIndex = 5;
+			prevUnannotate.Text = "PrevUnannotate (U)";
+			prevUnannotate.UseVisualStyleBackColor = true;
+			prevUnannotate.Click += prevUnannotate_Click;
+			// 
+			// prev
+			// 
+			prev.Location = new Point(12, 546);
+			prev.Name = "prev";
+			prev.Size = new Size(138, 23);
+			prev.TabIndex = 6;
+			prev.Text = "Prev (J)";
+			prev.UseVisualStyleBackColor = true;
+			prev.Click += prev_Click;
+			// 
+			// save
+			// 
+			save.Location = new Point(275, 517);
+			save.Name = "save";
+			save.Size = new Size(138, 23);
+			save.TabIndex = 7;
+			save.Text = "Save (Enter)";
+			save.UseVisualStyleBackColor = true;
+			save.Click += save_Click;
+			// 
+			// export
+			// 
+			export.Location = new Point(275, 546);
+			export.Name = "export";
+			export.Size = new Size(138, 23);
+			export.TabIndex = 8;
+			export.Text = "Export";
+			export.UseVisualStyleBackColor = true;
+			export.Click += export_Click;
+			// 
+			// subtitleLabel
+			// 
+			subtitleLabel.AutoSize = true;
+			subtitleLabel.Location = new Point(309, 385);
+			subtitleLabel.Name = "subtitleLabel";
+			subtitleLabel.Size = new Size(79, 15);
+			subtitleLabel.TabIndex = 10;
+			subtitleLabel.Text = "subtitleLabel";
+			// 
+			// frameLabel
+			// 
+			frameLabel.AutoSize = true;
+			frameLabel.Location = new Point(399, 423);
+			frameLabel.Name = "frameLabel";
+			frameLabel.Size = new Size(77, 15);
+			frameLabel.TabIndex = 12;
+			frameLabel.Text = "(<)frame(>):";
+			// 
+			// anotateStatus
+			// 
+			anotateStatus.AutoSize = true;
+			anotateStatus.Location = new Point(309, 455);
+			anotateStatus.Name = "anotateStatus";
+			anotateStatus.Size = new Size(85, 15);
+			anotateStatus.TabIndex = 13;
+			anotateStatus.Text = "anotateStatus";
+			// 
+			// numericUpDown1
+			// 
+			numericUpDown1.Location = new Point(482, 421);
+			numericUpDown1.Maximum = new decimal(new int[] { 130000, 0, 0, 0 });
+			numericUpDown1.Name = "numericUpDown1";
+			numericUpDown1.Size = new Size(120, 23);
+			numericUpDown1.TabIndex = 14;
+			numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			AutoSize = true;
+			ClientSize = new Size(662, 581);
+			Controls.Add(numericUpDown1);
+			Controls.Add(anotateStatus);
+			Controls.Add(frameLabel);
+			Controls.Add(subtitleLabel);
+			Controls.Add(export);
+			Controls.Add(save);
+			Controls.Add(prev);
+			Controls.Add(prevUnannotate);
+			Controls.Add(nextUnannotate);
+			Controls.Add(next);
+			Controls.Add(picture);
+			FormBorderStyle = FormBorderStyle.FixedDialog;
+			MaximizeBox = false;
+			MinimizeBox = false;
+			Name = "Form1";
+			Text = "Form1";
+			FormClosing += Form1_FormClosing;
+			Load += Form1_Load;
+			KeyDown += Form1_KeyDown;
+			((System.ComponentModel.ISupportInitialize)picture).EndInit();
+			((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private PictureBox picture;
-        private Button button1;
-        private Button next;
-        private Button nextUnannotate;
-        private Button prevUnannotate;
-        private Button prev;
-        private Button save;
-        private Button export;
-    }
+		private PictureBox picture;
+		private Button button1;
+		private Button next;
+		private Button nextUnannotate;
+		private Button prevUnannotate;
+		private Button prev;
+		private Button save;
+		private Button export;
+		private Label subtitleLabel;
+		private Label frameLabel;
+		private Label anotateStatus;
+		private NumericUpDown numericUpDown1;
+	}
 }
--- Form1.cs
+++ Form1.cs
@@ -3,209 +3,249 @@ using System.Runtime.CompilerServices;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Windows.Forms;
 
 namespace MyPic_Annotator
 {
-    public partial class Form1 : Form
-    {
-        JsonSerializerOptions jsonSerializerOptions = new()
-        {
-            WriteIndented = true,
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-        };
-        private readonly List<Data> data = [];
-        private readonly List<string> character = ["高松燈", "千早愛音", "要樂奈", "長崎爽世", "椎名立希", "三角初華", "豐川祥子", "八幡海鈴", "祐天寺若麥", "若葉睦"];
-        private readonly List<CheckBox> characterBox;
-        int currnetIndex = 0;
-        bool unsaved = false;
-        private readonly List<Keys> keys = [Keys.Q, Keys.W, Keys.E, Keys.R, Keys.T, Keys.A, Keys.S, Keys.D, Keys.F, Keys.G];
-        public Form1()
-        {
-            InitializeComponent();
-            if (File.Exists("data.json"))
-                data = JsonSerializer.Deserialize<List<Data>>(File.ReadAllText($"data.json")) ?? [];
-            else
-            {
-                MessageBox.Show("data.json not found. Please load the data file.");
-                OpenFileDialog openFileDialog = new()
-                {
-                    Filter = "JSON files (*.json)|*.json",
-                    Title = "Open an JSON File"
-                };
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    data = JsonSerializer.Deserialize<List<Data>>(File.ReadAllText(openFileDialog.FileName)) ?? [];
-                }
-            }
-            characterBox = [];
-            for (int i = 0; i < character.Count; i++)
-            {
-                int x = i < 5 ? 20 : 140;
-                int y = 401 + (i % 5) * 20;
-                CheckBox checkBox = new()
-                {
-                    Text = $"{character[i]} ({keys[i].ToString()})",
-                    Location = new Point(x, y),
-                    AutoSize = true,
-                    TabStop = false
-                };
-                Controls.Add(checkBox);
-                characterBox.Add(checkBox);
-            }
-        }
+	public partial class Form1 : Form
+	{
+		JsonSerializerOptions jsonSerializerOptions = new()
+		{
+			WriteIndented = true,
+			Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+		};
+		private readonly List<Data> data = [];
+		private readonly List<string> character = ["高松燈", "千早愛音", "要樂奈", "長崎爽世", "椎名立希", "三角初華", "豐川祥子", "八幡海鈴", "祐天寺若麥", "若葉睦"];
+		private readonly List<CheckBox> characterBox;
+		int currnetIndex = 0;
+		bool unsaved = false;
+		private readonly List<Keys> keys = [Keys.Q, Keys.W, Keys.E, Keys.R, Keys.T, Keys.A, Keys.S, Keys.D, Keys.F, Keys.G];
+		public Form1()
+		{
+			InitializeComponent();
+			if (File.Exists("data.json"))
+				data = JsonSerializer.Deserialize<List<Data>>(File.ReadAllText($"data.json")) ?? [];
+			else
+			{
+				MessageBox.Show("data.json not found. Please load the data file.");
+				OpenFileDialog openFileDialog = new()
+				{
+					Filter = "JSON files (*.json)|*.json",
+					Title = "Open an JSON File"
+				};
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					data = JsonSerializer.Deserialize<List<Data>>(File.ReadAllText(openFileDialog.FileName)) ?? [];
+				}
+			}
+			characterBox = [];
+			for (int i = 0; i < character.Count; i++)
+			{
+				int x = i < 5 ? 20 : 140;
+				int y = 401 + (i % 5) * 20;
+				CheckBox checkBox = new()
+				{
+					Text = $"{character[i]} ({keys[i]})",
+					Location = new Point(x, y),
+					AutoSize = true,
+					TabStop = false
+				};
+				Controls.Add(checkBox);
+				characterBox.Add(checkBox);
+			}
+		}
 
-        private void LoadImage(string session, int frame)
-        {
-            picture.Load($"https://mygodata.0m0.uk/images/{session}_{frame}.jpg");
-        }
+		private void LoadImage()
+		{
+			var session = data[currnetIndex].episode;
+			var frame = data[currnetIndex].frame_prefer;
+			picture.Load($"https://qwer.0m0.uk/images/{session}_{frame}.jpg");
+		}
 
-        private void LoadCurrentData()
-        {
-            LoadImage(data[currnetIndex].episode, data[currnetIndex].frame_start);
-            if (data[currnetIndex].character != 0)
-            {
-                for (int i = 0; i < characterBox.Count; i++)
-                {
-                    characterBox[i].Checked = (data[currnetIndex].character & (1 << i)) != 0;
-                }
-            }
-        }
+		private void LoadCurrentData()
+		{
+			if (data[currnetIndex].frame_prefer == 0)
+			{
+				data[currnetIndex].frame_prefer = (data[currnetIndex].frame_start + data[currnetIndex].frame_end) / 2;
+			}
+			LoadImage();
+			subtitleLabel.Text = data[currnetIndex].text;
+			numericUpDown1.Value = data[currnetIndex].frame_prefer;
+			if (data[currnetIndex].character != 0)
+			{
+				for (int i = 0; i < characterBox.Count; i++)
+				{
+					characterBox[i].Checked = (data[currnetIndex].character & (1 << i)) != 0;
+				}
+				anotateStatus.Text = "Annotated";
+				anotateStatus.ForeColor = Color.Green;
+			}
+			else
+			{
+				anotateStatus.Text = "Unannotated";
+				anotateStatus.ForeColor = Color.Red;
+			}
+		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            KeyPreview = true;
-            foreach (Control ctrl in Controls)
-            {
-                ctrl.TabStop = false; // 全部控制項關閉 Tab 選取
-            }
-            LoadCurrentData();
-        }
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			KeyPreview = true;
+			foreach (Control ctrl in Controls)
+			{
+				ctrl.TabStop = false; // 全部控制項關閉 Tab 選取
+			}
+			numericUpDown1.Enabled = false;
+			LoadCurrentData();
+		}
 
-        private void save_Click(object sender, EventArgs e)
-        {
-            int val = 0;
-            for (int i = 0; i < characterBox.Count; i++)
-            {
-                if (characterBox[i].Checked)
-                {
-                    val += 1 << i;
-                }
-            }
-            data[currnetIndex].character = val;
-            unsaved = true;
-            Debug.WriteLine(val);
-        }
+		private void save_Click(object sender, EventArgs e)
+		{
+			int val = 0;
+			for (int i = 0; i < characterBox.Count; i++)
+			{
+				if (characterBox[i].Checked)
+				{
+					val += 1 << i;
+				}
+			}
+			data[currnetIndex].character = val;
+			if (val == 0)
+			{
+				anotateStatus.Text = "Unannotated";
+				anotateStatus.ForeColor = Color.Red;
+			}
+			else
+			{
+				anotateStatus.Text = "Annotated";
+				anotateStatus.ForeColor = Color.Green;
+			}
+			unsaved = false;
+			Debug.WriteLine(val);
+		}
 
-        private void next_Click(object sender, EventArgs e)
-        {
-            if (currnetIndex < data.Count - 1)
-            {
-                currnetIndex++;
-                LoadCurrentData();
-            }
-        }
+		private void next_Click(object sender, EventArgs e)
+		{
+			if (currnetIndex < data.Count - 1)
+			{
+				currnetIndex++;
+				LoadCurrentData();
+			}
+		}
 
-        private void nextUnannotate_Click(object sender, EventArgs e)
-        {
-            while (currnetIndex < data.Count - 1)
-            {
-                currnetIndex++;
-                if (data[currnetIndex].character == 0)
-                {
-                    LoadCurrentData();
-                    return;
-                }
-            }
-            DialogResult result = MessageBox.Show("All images are annotated. Do you want to save the data?", "Info", MessageBoxButtons.OK);
+		private void nextUnannotate_Click(object sender, EventArgs e)
+		{
+			while (currnetIndex < data.Count - 1)
+			{
+				currnetIndex++;
+				if (data[currnetIndex].character == 0)
+				{
+					LoadCurrentData();
+					return;
+				}
+			}
+			DialogResult result = MessageBox.Show("All images are annotated. Do you want to save the data?", "Info", MessageBoxButtons.OK);
 
-        }
+		}
 
-        private void prev_Click(object sender, EventArgs e)
-        {
-            if (currnetIndex > 0)
-            {
-                currnetIndex--;
-                LoadCurrentData();
-            }
-        }
+		private void prev_Click(object sender, EventArgs e)
+		{
+			if (currnetIndex > 0)
+			{
+				currnetIndex--;
+				LoadCurrentData();
+			}
+		}
 
-        private void prevUnannotate_Click(object sender, EventArgs e)
-        {
-            while (currnetIndex > 0)
-            {
-                currnetIndex--;
-                if (data[currnetIndex].character == 0)
-                {
-                    LoadCurrentData();
-                    return;
-                }
-            }
-            DialogResult result = MessageBox.Show("All images are annotated. Do you want to save the data?", "Info", MessageBoxButtons.OK);
+		private void prevUnannotate_Click(object sender, EventArgs e)
+		{
+			while (currnetIndex > 0)
+			{
+				currnetIndex--;
+				if (data[currnetIndex].character == 0)
+				{
+					LoadCurrentData();
+					return;
+				}
+			}
+			DialogResult result = MessageBox.Show("All images are annotated. Do you want to save the data?", "Info", MessageBoxButtons.OK);
 
-        }
+		}
 
-        private void export_Click(object sender, EventArgs e)
-        {
-            // promote user to select save location
-            SaveFileDialog saveFileDialog = new()
-            {
-                Filter = "JSON files (*.json)|*.json",
-                Title = "Save an JSON File",
-                FileName = "new_data.json"
-            };
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                File.WriteAllText(saveFileDialog.FileName, JsonSerializer.Serialize(data, jsonSerializerOptions));
-            }
+		private void export_Click(object sender, EventArgs e)
+		{
+			// promote user to select save location
+			SaveFileDialog saveFileDialog = new()
+			{
+				Filter = "JSON files (*.json)|*.json",
+				Title = "Save an JSON File",
+				FileName = "new_data.json"
+			};
+			if (saveFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				File.WriteAllText(saveFileDialog.FileName, JsonSerializer.Serialize(data, jsonSerializerOptions));
+			}
 
-        }
+		}
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if (unsaved)
-            {
-                DialogResult result = MessageBox.Show("Do you want to save the data?", "Info", MessageBoxButtons.YesNoCancel);
-                if (result == DialogResult.Yes)
-                {
-                    export_Click(sender, e);
-                }
-                else if (result == DialogResult.Cancel)
-                {
-                    e.Cancel = true;
-                }
-            }
-        }
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (unsaved)
+			{
+				DialogResult result = MessageBox.Show("Do you want to save the data?", "Info", MessageBoxButtons.YesNoCancel);
+				if (result == DialogResult.Yes)
+				{
+					export_Click(sender, e);
+				}
+				else if (result == DialogResult.Cancel)
+				{
+					e.Cancel = true;
+				}
+			}
+		}
 
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            for (int i = 0; i < keys.Count(); i++)
-            {
-                if (e.KeyCode == keys[i])
-                {
-                    characterBox[i].Checked = !characterBox[i].Checked;
-                }
-            }
-            switch (e.KeyCode)
-            {
-                case Keys.L:
-                    next.PerformClick(); break;
-                case Keys.J:
-                    prev.PerformClick(); break;
-                case Keys.O:
-                    nextUnannotate.PerformClick(); break;
-                case Keys.U:
-                    prevUnannotate.PerformClick(); break;
-            }
-        }
-    }
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			switch (e.KeyCode)
+			{
+				case Keys.L:
+					next.PerformClick(); break;
+				case Keys.J:
+					prev.PerformClick(); break;
+				case Keys.O:
+					nextUnannotate.PerformClick(); break;
+				case Keys.U:
+					prevUnannotate.PerformClick(); break;
+				case Keys.OemPeriod:
+					numericUpDown1.UpButton(); break;
+				case Keys.Oemcomma:
+					numericUpDown1.DownButton(); break;
+				case Keys.Enter:
+					save.PerformClick(); break;
+				default:
+					var idx = keys.IndexOf(e.KeyCode);
+					if (idx != -1)
+						characterBox[idx].Checked = !characterBox[idx].Checked;
+					break;
+			}
+		}
 
-    class Data
-    {
-        public required string text { get; set; }
-        public required string episode { get; set; }
-        public int frame_start { get; set; }
-        public int frame_end { get; set; }
-        public int segment_id { get; set; }
-        public int character { get; set; }
-    }
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+			unsaved = true;
+			data[currnetIndex].frame_prefer = Convert.ToInt32(numericUpDown1.Value);
+			Debug.WriteLine(numericUpDown1.Value);
+			LoadCurrentData();
+		}
+	}
+
+	class Data
+	{
+		public required string text { get; set; }
+		public required string episode { get; set; }
+		public int frame_start { get; set; }
+		public int frame_prefer { get; set; }
+		public int frame_end { get; set; }
+		public int segment_id { get; set; }
+		public int character { get; set; }
+	}
 }
