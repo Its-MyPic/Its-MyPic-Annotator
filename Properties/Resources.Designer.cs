7800bfbd8176c8e8b210a626240f64a068ead565
jeffpeng3 <jeffpeng2012@gmail.com>
2025-02-10 03:59:54 +0800

    chore: rename to anontator

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MyPic_Annotator.Properties {
+namespace MyPic_Anontator.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MyPic_Annotator.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MyPic_Annotator.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MyPic_Anontator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
